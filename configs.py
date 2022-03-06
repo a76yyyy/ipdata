f4f2464dab77898a5ba2f96a0dbc1b808dcdc0a1
a76yyyy <q981331502@163.com>
2022-03-06 23:14:22 +0800

    因ipv6停止开放离线镜像, 默认禁用v6数据库更新

--- configs.py
+++ configs.py
@@ -9,6 +9,7 @@
 import os
 
 default_dat_update = True #执行ip_Sync.py时是否默认联网自动更新czipdata.dat文件, False为默认不更新, 不存在dat文件时该选项失效。
+default_v6dat_update = False #执行ip_Sync.py时是否默认联网自动更新ipv6wry.db文件, False为默认不更新, 不存在dat文件时该选项失效。
 default_sqlite3_update = True #当执行ip_Sync.py时是否默认自动更新SQLite3数据库, False为默认不更新。
 default_txt_update = False #当数据文件版本无更新时是否默认自动更新czipdata.txt文件, False为默认不更新。
 default_sql_update = True #当执行ip_Sync.py时是否默认自动更新数据库, False为默认不更新。
